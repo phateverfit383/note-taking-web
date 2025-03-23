915910a562145d022b2c4b5cc414a87382f336ff
Phat(BE) <phatnguyen@everfit.io>
2025-03-24 05:01:13 +0700

    fix: redirect to login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
-"use client";
+'use client';
 
-import Head from "next/head";
-import * as React from "react";
-import "@/lib/env";
+import Head from 'next/head';
+import * as React from 'react';
+import '@/lib/env';
 
-import ArrowLink from "@/components/links/ArrowLink";
-import ButtonLink from "@/components/links/ButtonLink";
-import UnderlineLink from "@/components/links/UnderlineLink";
-import UnstyledLink from "@/components/links/UnstyledLink";
+import ArrowLink from '@/components/links/ArrowLink';
+import ButtonLink from '@/components/links/ButtonLink';
+import UnderlineLink from '@/components/links/UnderlineLink';
+import UnstyledLink from '@/components/links/UnstyledLink';
 
 /**
  * SVGR Support
@@ -16,22 +16,22 @@ import UnstyledLink from "@/components/links/UnstyledLink";
  * You can override the next-env if the type is important to you
  * @see https://stackoverflow.com/questions/68103844/how-to-override-next-js-svg-module-declaration
  */
-import Logo from "~/svg/Logo.svg";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import Logo from '~/svg/Logo.svg';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function HomePage() {
-  const [mode, setMode] = React.useState<"dark" | "light">("dark");
+  const [mode, setMode] = React.useState<'dark' | 'light'>('dark');
 
   function toggleMode() {
-    return mode === "dark" ? setMode("light") : setMode("dark");
+    return mode === 'dark' ? setMode('light') : setMode('dark');
   }
 
   const router = useRouter();
 
   // redirect to notes page
   useEffect(() => {
-    router.push("/notes");
+    router.push('/login');
   }, []);
 
   return <></>;
