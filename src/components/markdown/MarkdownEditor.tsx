77cb8216e5adfbfe93280b320273c38fee6734b6
Phat(BE) <phatnguyen@everfit.io>
2025-03-23 21:16:46 +0700

    fix: handle error throw error when the component not render

--- src/components/markdown/MarkdownEditor.tsx
+++ src/components/markdown/MarkdownEditor.tsx
@@ -1,19 +1,38 @@
-import React, { useState } from "react";
 import { MdEditor } from "md-editor-rt";
 import "md-editor-rt/lib/style.css";
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 
-type Props = {
+type MarkdownEditorProps = {
+  children?: React.ReactNode;
   text: string;
   onChange: (text: string) => void;
 };
 
-export default (props: Props) => {
-  return (
-    <MdEditor
-      value={props.text}
-      onChange={props.onChange}
-      theme="dark"
-      language="en-US"
-    />
-  );
-};
+const MarkdownEditor = React.forwardRef<HTMLButtonElement, MarkdownEditorProps>(
+  ({ children, text, onChange }, ref) => {
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+      setIsMounted(true);
+    }, []);
+    if (!isMounted) {
+      return <div>Loading editor...</div>;
+    }
+    return (
+      <div className="w-full h-full">
+        <MdEditor
+          value={text}
+          onChange={onChange}
+          theme="dark"
+          language="en-US"
+        />
+
+        {children}
+      </div>
+    );
+  }
+);
+
+export default MarkdownEditor;
