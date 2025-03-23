e1cdc27141199c43b513d3cffacca37fdda1387e
Phat(BE) <phatnguyen@everfit.io>
2025-03-24 01:12:12 +0700

    fix: handle error of api service

--- src/app/api/apiService.ts
+++ src/app/api/apiService.ts
@@ -8,22 +8,50 @@ export class ApiService {
   }
 
   get = async (route: string, params: any) => {
-    const res = await axios.get(`${this.baseUrl}${route}`, { params });
-    return res.data;
+    try {
+      const res = await axios.get(`${this.baseUrl}${route}`, { params });
+      return res.data;
+    } catch (err: any) {
+      return this.handleError(err);
+    }
   };
 
   post = async (route: string, id: string, data: any) => {
-    const res = await axios.post(`${this.baseUrl}${route}/${id}`, data);
-    return res.data;
+    try {
+      const res = await axios.post(`${this.baseUrl}${route}/${id}`, data);
+      return res.data;
+    } catch (err: any) {
+      return this.handleError(err);
+    }
   };
 
   put = async (route: string, id: string, data: any) => {
-    const res = await axios.put(`${this.baseUrl}${route}/${id}`, data);
-    return res.data;
+    try {
+      const res = await axios.put(`${this.baseUrl}${route}/${id}`, data);
+      return res.data;
+    } catch (err: any) {
+      return this.handleError(err);
+    }
   };
 
   delete = async (route: string, id: string) => {
-    const res = await axios.delete(`${this.baseUrl}${route}/${id}`);
-    return res.data;
+    try {
+      const res = await axios.delete(`${this.baseUrl}${route}/${id}`);
+      return res.data;
+    } catch (err: any) {
+      return this.handleError(err);
+    }
+  };
+
+  handleError = (err: any) => {
+    const { response } = err;
+    if (response.status === 401) {
+      // @TODO: handle redirect to login page
+      return { success: false, message: "Unauthorized" };
+    } else if (response.status === 400) {
+      return { success: false, message: response.data.message };
+    } else if (response.status === 500) {
+      return { success: false, message: "Internal server error" };
+    }
   };
 }
